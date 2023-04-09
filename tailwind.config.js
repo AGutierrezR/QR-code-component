2cfc5fdd8c00fc844ede737f85266821a5997c8a
Andres Gutierrez <agutierrez.codes@gmail.com>
2023-04-10 00:32:45 +0200

    Add project config

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Outfit', 'san-serif'],
+      },
+      fontSize: {
+        base: '15px',
+      },
+      colors: {
+        white: 'hsl(0, 0%, 100%)',
+        'light-gray': 'hsl(212, 45%, 89%)',
+        'grayish-blue': 'hsl(220, 15%, 55%)',
+        'dark-blue': 'hsl(218, 44%, 22%)',
+      },
+    },
+  },
+  plugins: [],
+}
